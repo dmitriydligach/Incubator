d1400be989ed4f46995efdbfe8346f83b0787085
Dima <ddligach@luc.edu>
2020-06-26 12:46:14 -0500

    added a method to convert texts to ints

--- Lib/tokenizer.py
+++ Lib/tokenizer.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
-import glob, collections, os
+import collections, os
 
 class Tokenizer:
   """Tokenization and vectorization"""
 
-  def __init__(self, n_words, lower=False):
+  def __init__(self, n_words, lower=False, oov_token='oovtok'):
     """Constructiion deconstruction"""
 
     self.n_words = n_words
     self.lower = lower
+    self.oov = oov_token
     self.stoi = {}
     self.itos = {}
+
+    self.stoi[oov_token] = 0
+    self.itos[0] = oov_token
 
   def fit_on_texts(self, texts):
     """Fit on a list of documents"""
@@ -22,13 +26,28 @@ class Tokenizer:
       tokens = text.split()
       counts.update(tokens)
 
-    # zero is reserved for something
     index = 1
-
     for token, _ in counts.most_common(self.n_words):
       self.stoi[token] = index
       self.itos[index] = token
       index += 1
+
+  def texts_to_sequences(self, texts):
+    """List of strings to list of int sequences"""
+
+    sequences = []
+    for text in texts:
+
+      sequence = []
+      for token in text.split():
+        if token in self.stoi:
+          sequence.append(self.stoi[token])
+        else:
+          sequence.append(self.stoi[self.oov])
+
+      sequences.append(sequence)
+
+    return sequences
 
 class DirDataReader:
   """Each label is in a subdirectory"""
@@ -58,7 +77,16 @@ if __name__ == "__main__":
     '/Users/Dima/Work/Data/Opioids/Train/',
     {'no':0, 'yes':1})
 
-  tokenizer = Tokenizer(n_words=100)
-  tokenizer.fit_on_texts(samples)
+  texts = ['it is happening again',
+           'the owls are not what they seem',
+           'again and again',
+           'the owls are happening']
+
+  tokenizer = Tokenizer(n_words=5)
+
+  tokenizer.fit_on_texts(texts)
   print(tokenizer.stoi)
   print(tokenizer.itos)
+
+  seqs = tokenizer.texts_to_sequences(texts)
+  print(seqs)
